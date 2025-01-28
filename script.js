6d5a40752f79a96542747bf753772b64f2bae83b
Ashish Kumar Atul <162558245+Ashish-Kumar-Atul@users.noreply.github.com>
2025-01-28 18:10:02 +0530

    Added Enter to add task

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ submit.addEventListener('click',()=>{
         alert('Enter a Task!');
         return;
     }
+
     alltasks.innerHTML += `
         <label class="my-2 flex items-center space-x-2">
             <input type="checkbox" name="task1" id="task1">
@@ -17,10 +18,32 @@ submit.addEventListener('click',()=>{
     `;
     addtask.value = '';
 });
+addtask.addEventListener('keydown',(e)=>{
+    if(e.key === "Enter"){
+        const task = addtask.value;
+        if(!task){
+            alert('Enter a Task!');
+            return;
+        }
+        
+        alltasks.innerHTML += `
+            <label class="my-2 flex items-center space-x-2">
+                <input type="checkbox" name="task1" id="task1">
+                <span class="w-[21vw] ">${task}</span>
+                <button id="delete" class="border-2 text-black bg-yellow-600 rounded-xl px-2" type="delete">Delete</button>
+            </label>
+        `;
+        addtask.value = '';
+    }
+});
 
 alltasks.addEventListener('click',(e)=>{
     if(e.target.tagName=='BUTTON'){
         const deletetask = e.target.parentElement;
         deletetask.remove();
+    }
+    if (target.classList.contains('task-checkbox')) {
+        const taskText = e.target.nextElementSibling;
+        alltasks.style.backgroundColor = "green";
     }
 });
